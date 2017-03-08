d6e68e552a9d4856c63790c250bdb3c29594e586
Thomas Kwak <teekwak@gmail.com>
2017-03-07 18:06:04 -0800

    MySQL connection to database using POST method

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const handlebars = require('express-handlebars')
+const handlebars = require('express-handlebars');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const credentials = require('./credentials');
+
 const app = express();
 
 // redirect bootstrap and jquery dependencies
@@ -11,9 +15,35 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// parse form information
+app.use(bodyParser.urlencoded({extended: true}))
+
 // redirect '/'
 app.get('/', function (req, res) {
     res.render('home');
+});
+
+app.post('/search', function(req, res) {
+	console.log(req.body.query);
+
+	var connection = mysql.createConnection({
+	  host     : credentials.host,
+	  user     : credentials.username,
+	  password : credentials.password,
+	  database : credentials.database
+	});
+
+	connection.connect();
+
+	connection.query('SELECT COUNT(*) FROM Pages', function (err, rows, fields) {
+	  if (err) throw err;
+
+	  console.log('The output is: ', rows[0]);
+	})
+
+	connection.end();
+
+	res.render('home');
 });
 
 // start app on port 3000
